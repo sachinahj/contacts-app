386e9f662dc13d86e0fbcc8ca46a070ee440f9b9
Sachin Ahuja <sachinahj@gmail.com>
2017-09-07 19:23:44 -0700

    more udpates

---
+++ ContactsApp/ExtensionsUI.swift
@@ -0,0 +1,29 @@
+//
+//  ExtensionsUI.swift
+//  ContactsApp
+//
+//  Created by Sachin Ahuja on 9/7/17.
+//  Copyright © 2017 Sachin Ahuja. All rights reserved.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    func shake(count : Float? = nil,for duration : TimeInterval? = nil,withTranslation translation : Float? = nil) {
+        
+        // You can change these values, so that you won't have to write a long function
+        let defaultRepeatCount: Float = 4.0
+        let defaultTotalDuration: TimeInterval = 0.5
+        let defaultTranslation = -5
+        
+        let animation : CABasicAnimation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        
+        animation.repeatCount = count ?? defaultRepeatCount
+        animation.duration = (duration ?? defaultTotalDuration)/TimeInterval(animation.repeatCount)
+        animation.autoreverses = true
+        animation.byValue = translation ?? defaultTranslation
+        layer.add(animation, forKey: "shake")
+    }    
+}
--- ContactsApp/SplashController.swift
+++ ContactsApp/SplashController.swift
@@ -12,20 +12,35 @@ class SplashController: UIViewController {
 
     var username: String?
     @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var jumpIn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("SplashController")
+        
+        self.title = "Enter a Username"
+        jumpIn.layer.cornerRadius = 10.0
+        jumpIn.clipsToBounds = true
     }
     
-    @IBAction func jumpIn(_ sender: UIButton) {
-        if usernameField.text != "" {
-            username = usernameField.text?.trimmingCharacters(in: .whitespaces)
-            view.endEditing(true)
-            performSegue(withIdentifier: "goToMap", sender: self)
+    @IBAction func usernameFieldOnChange(_ sender: UITextField) {
+        if usernameField.text! != "" {
+            self.title = "Hello \(usernameField.text!)!"
+        } else {
+            self.title = "Enter a Username"
         }
     }
     
+    @IBAction func jumpInOnClick(_ sender: UIButton) {
+        if usernameField.text! != "" {
+            username = usernameField.text?.trimmingCharacters(in: .whitespaces)
+            view.endEditing(true)
+            performSegue(withIdentifier: "goToMap", sender: self)
+        } else {
+            usernameField.shake()
+        }
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToMap" {
             let destinationVC = segue.destination as! MapController
