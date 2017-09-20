c5503ae27bb585738626ea85c37af89a1c328e37
Atom - Sachin <sachin@atomtickets.com>
2017-09-20 14:56:41 -0700

    fix inactive handling

--- ContactsApp/AppDelegate.swift
+++ ContactsApp/AppDelegate.swift
@@ -14,32 +14,39 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var timer: Timer?
+    
+    @objc func resetApp() {
+        print("AppDelegate: resetApp")
+        DBManager.removeMe()
+        let navigationController = self.window?.rootViewController as! UINavigationController
+        navigationController.popToRootViewController(animated: false)
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         GMSServices.provideAPIKey("AIzaSyDEfRVItCbvADthIQur3Sh7ardHBwbNwjY")
         FirebaseApp.configure()
-        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-
-        DBManager.removeMe()
         print("AppDelegate: applicationWillResignActive")
+        timer = Timer.scheduledTimer(
+            timeInterval: 60,
+            target: self,
+            selector: #selector(self.resetApp),
+            userInfo: nil,
+            repeats: false)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        
-        DBManager.removeMe()
-        let navigationController = self.window?.rootViewController as! UINavigationController
-        navigationController.popToRootViewController(animated: false)
         print("AppDelegate: applicationDidEnterBackground")
+        resetApp()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -48,15 +55,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("AppDelegate: applicationDidBecomeActive")
+        timer?.invalidate()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        
-        DBManager.removeMe()
-        let navigationController = self.window?.rootViewController as! UINavigationController
-        navigationController.popToRootViewController(animated: false)
         print("AppDelegate: applicationWillTerminate")
+        resetApp()
     }
 
 }
